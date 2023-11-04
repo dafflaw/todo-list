dc0a3034e3306c0ca05fc89f60e21e816d776fe6
omad_d <odilshodvekov13@gmail.com>
2023-11-05 01:35:30 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,267 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useLocalStorage = (storageKey, fallbackState) => {
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem(storageKey)) ?? fallbackState
+  )
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(value))
+  }, [value, storageKey])
+
+  return [value, setValue]
 }
 
-export default App;
+export default function App() {
+  const [tasksArray, setTasksArray] = useLocalStorage("notes", [])
+  const [status, setStatus] = useState("all")
+
+  // useEffect(() => {
+  //   localStorage.setItem("notes", JSON.stringify(tasksArray))
+  // }, [tasksArray])
+
+  function handleClear() {
+    setTasksArray([])
+    localStorage.clear()
+  }
+
+  function handleChangeTasksArray(text) {
+    setTasksArray(() => tasksArray.filter((cur) => cur?.text !== text))
+  }
+
+  function handleAddTask(obj) {
+    setTasksArray((arr) => [obj, ...arr])
+  }
+
+  return (
+    <div className="wrapper">
+      <Heading />
+      <AddTask
+        onAddTask={handleAddTask}
+        onStatus={setStatus}
+        tasks={tasksArray}
+        changeTasks={setTasksArray}
+      />
+      <TaskStatus onClick={handleClear} onSetStatus={setStatus} />
+      <TaskList
+        tasks={tasksArray}
+        changeTasks={handleChangeTasksArray}
+        status={status}
+      />
+    </div>
+  )
+}
+
+function Heading() {
+  return <h2 className="heading">What's the Plan for Today?</h2>
+}
+
+function AddTask({ onAddTask, onStatus }) {
+  // const initialArr = localStorage.getItem("notes")
+  // if (initialArr) changeTasks(() => JSON.parse(initialArr))
+  const [task, setTask] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!task || !task.replace(/\s/g, "").length) return
+
+    const newItem = {
+      text: task,
+      id: Date.now(),
+      status: "pending",
+      checked: false,
+    }
+
+    onAddTask(newItem)
+    onStatus("all")
+    setTask("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        type="text"
+        className="add--task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <label className="add--task__plus" onClick={handleSubmit}>
+        <ion-icon
+          name="add-outline"
+          className="add--task__plus-icon"
+        ></ion-icon>
+      </label>
+    </form>
+  )
+}
+
+function TaskStatus({ onClick, onSetStatus }) {
+  return (
+    <ul className="task--status__list">
+      <button onClick={() => onSetStatus("all")}>All</button>
+      <button onClick={() => onSetStatus("pending")}>Pending</button>
+      <button onClick={() => onSetStatus("completed")}>Completed</button>
+      <button onClick={() => onClick()}>Clear All</button>
+    </ul>
+  )
+}
+
+function TaskList({ tasks, changeTasks, status }) {
+  const pendingArr = tasks?.filter((task) => task.status === "pending")
+  const completedArr = tasks?.filter((task) => task.status === "completed")
+  // let message = ""
+  // if (completedArr.length === 0) message = `You don't have here any task yet!`
+
+  return (
+    <ul className="tasks-list">
+      {status === "all" ? (
+        tasks.length === 0 ? (
+          <p>You don't have any tasks yet!</p>
+        ) : (
+          tasks?.map((task) => (
+            <Task
+              task={task}
+              changeTasks={changeTasks}
+              tasks={tasks}
+              key={task.id}
+            />
+          ))
+        )
+      ) : (
+        ""
+      )}
+      {status === "pending" ? (
+        pendingArr.length === 0 ? (
+          <p>You don't have any tasks pending!</p>
+        ) : (
+          pendingArr.map((task) => (
+            <Task
+              task={task}
+              changeTasks={changeTasks}
+              tasks={tasks}
+              key={task.id}
+            />
+          ))
+        )
+      ) : (
+        ""
+      )}
+      {status === "completed" ? (
+        completedArr.length === 0 ? (
+          <p>You don't have any tasks completed!</p>
+        ) : (
+          completedArr.map((task) => (
+            <Task
+              task={task}
+              changeTasks={changeTasks}
+              tasks={tasks}
+              key={task.id}
+            />
+          ))
+        )
+      ) : (
+        ""
+      )}
+    </ul>
+  )
+}
+
+function Task({ task, changeTasks, tasks }) {
+  const [checked, setChecked] = useState(task.checked)
+  const [changesTabs, setChangesTabs] = useState(false)
+  const [editing, setEditing] = useState(false)
+  const [editText, setEditText] = useState(task.text)
+
+  task.checked = checked
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(tasks))
+  }, [tasks])
+
+  if (checked) task.status = "completed"
+  if (!checked) task.status = "pending"
+
+  function handleEditing() {
+    setEditing(true)
+  }
+
+  function handleKeydown(target) {
+    setEditing(false)
+    setEditText(target.value)
+    task.text = editText
+  }
+
+  function handleCheckbox() {
+    setChecked(!checked)
+  }
+  // function handleInputChange() {
+  //   setEditText()
+  // }
+
+  return task.text.length > 0 ? (
+    <li>
+      <div className="task">
+        <input
+          type="checkbox"
+          value={checked}
+          checked={checked}
+          onChange={() => handleCheckbox()}
+        />
+
+        {editing ? (
+          <input
+            autoFocus
+            type="text"
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" ? handleKeydown(e.target) : ""
+            }
+            className="task--input"
+          />
+        ) : (
+          <p className={checked ? "task--text checked" : "task--text"}>
+            {task.text}
+          </p>
+        )}
+
+        <button
+          className="task--dots__button"
+          onClick={() => setChangesTabs(!changesTabs)}
+        >
+          <ion-icon
+            name="ellipsis-horizontal-outline"
+            className="task--dots__icon"
+          ></ion-icon>
+
+          {changesTabs ? (
+            <div className="task--changes">
+              <div className="task--change" onClick={() => handleEditing()}>
+                <ion-icon
+                  name="create-outline"
+                  className="task--changes__icon"
+                ></ion-icon>
+                Edit
+              </div>
+              <div
+                className="task--change"
+                onClick={() => changeTasks(task.text)}
+              >
+                <ion-icon
+                  name="trash-outline"
+                  className="task--changes__icon"
+                ></ion-icon>
+                Delete
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </button>
+      </div>
+    </li>
+  ) : (
+    ""
+  )
+}
